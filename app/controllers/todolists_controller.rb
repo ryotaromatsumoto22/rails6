8f368168b702b9d139d55d8824b55adad26060ff
ryotaro matsumoto <ryotaro@ryotaro-MacBook-Pro.local>
2020-05-10 12:41:15 +0900

    rails7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,15 +1,5 @@
 class TodolistsController < ApplicationController
 
-    def index
-        @lists = List.all
-    end
-
-    def show
-        @list = List.find(params[:id])
-    end
-
-
-
 
     def new
          @list = List.new
@@ -24,12 +14,30 @@ class TodolistsController < ApplicationController
          redirect_to todolist_path(list.id)
     end
 
+    def index
+        @lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id])
+    end
+
+    def edit
+        @list = List.find(params[:id])
+    end
+
+    def update
+        list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
+    end
+
 
 
     private
 
+    private
     def list_params
         params.require(:list).permit(:title, :body)
     end
-
 end
